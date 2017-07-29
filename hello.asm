44fda3d0adee0c203fd17dd22782ea5e23cd46a1
XCHGBXBX <xchg@protonmail.com>
2017-07-29 18:27:26 -0300

    Hello BIOS Worldw

--- hello.asm
+++ hello.asm
@@ -2,6 +2,33 @@
 ; hello.asm
 ; 28 July 2017
 ;
+	[BITS 16]
+	
+	ORG 0x7C00
+	
 
-	MOV EAX, 0xDEADBEEF
+	JMP main
+
+
+
+	MSG DB "Hello World!", 13, 10, 0
+
+
+printString:
+	LODSB
+	OR 	AL, AL
+	JZ	printStringRet
+	MOV 	AH, 0xE
+	INT 	0x10
+	JMP 	printString
+	
+printStringRet:
+	RET
+
+
+main:	
+	MOV 	SI, MSG
+	CALL	printString
+
+	CLI
 	HLT
